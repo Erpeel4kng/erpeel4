47ab25a7113cf43e96fc59c513028642377fa708
jicoding04@gmail.com <jicoding04@gmail.com>
2022-07-06 14:41:37 +0700

    added

--- js/popup.js
+++ js/popup.js
@@ -1,36 +1,26 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
+const openModalBtn = document.querySelectorAll('[data-modal-target]');
+const closeModalBtn = document.querySelectorAll('[data-close-btn]');
 
-openModalButtons.forEach(button => {
+openModalBtn.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
   });
 });
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active');
-  modal.forEach(modal => {
-    closeModal(modal);
-  });
-});
-
-closeModalButtons.forEach(button => {
+closeModalBtn.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal');
+    const modal = button.closest('#modal');
     closeModal(modal);
   });
 });
 
 function openModal(modal){
   if (modal == null) return
-  modal.classList.add('active');
-  overlay.classList.add('active');
+  modal.classList.add('open');
 };
 
 function closeModal(modal){
   if (modal == null) return
-  modal.classList.remove('active');
-  overlay.classList.remove('active');
+  modal.classList.remove('open');
 };
