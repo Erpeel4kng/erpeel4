c208afea6b16b4b221875cf62a38754b7e0ec7ce
jicoding04@gmail.com <jicoding04@gmail.com>
2022-07-08 17:08:42 +0700

    added

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,55 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".navMenu");
+const openNavMenu = document.querySelector(".open-nav-menu");
+const navMenu = document.querySelector(".nav-menu");
+const mediaSize = 991;
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+
+openNavMenu.addEventListener("click", toggleNav);
+
+openNavMenu.addEventListener("click", () => {
+  openNavMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+function toggleNav() {
+  navMenu.classList.toggle("open");
+}
+
+navMenu.addEventListener("click", (event) => {
+  if(event.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize){
+    event.preventDefault();
+    const menuItemHasChildren = event.target.parentElement;
+    
+    if(menuItemHasChildren.classList.contains("active")){
+      collapseSubMenu();
+    }
+    else{
+      if(navMenu.querySelector(".menu-item-has-children.active")){
+        collapseSubMenu();
+      }
+      
+      menuItemHasChildren.classList.add("active");
+      const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+      subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+    }
+  }
+});
+
+function collapseSubMenu(){
+  navMenu.querySelector(".menu-item-has-children.active .sub-menu").removeAttribute("style");
+  navMenu.querySelector(".menu-item-has-children.active").classList.remove("active");
+}
+
+function resizeFix(){
+  if(navMenu.classList.contains("open")){
+    toggleNav();
+  }
+  
+  if(navMenu.querySelector(".menu-item-has-children.active")){
+    collapseSubMenu();
+  }
+}
+
+window.addEventListener("resize", function(){
+  if(this.innerWidth > mediaSize){
+    resizeFix();
+  }
+})
